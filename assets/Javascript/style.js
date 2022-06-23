a54f5244651e2f0e4b637beffe112d3b7840a3f6
Nguyen Duc Dung <nguyenducdung13022001.nguyen@gmail.com>
2022-06-23 22:45:41 +0700

    Nguyen Duc Dung

---
+++ assets/Javascript/style.js
@@ -0,0 +1,23 @@
+// Ẩn hiện form login/Resgiter
+
+const modal = document.querySelector('.js-modal');
+console.log(modal)
+const modalLogin = document.querySelector('.js-btn-login');
+const modalBody = document.querySelector('.js-modal-body');
+console.log(modalLogin)
+
+function showFormLogin() {
+    modal.classList.add('open');
+}
+function hideFormLogin() {
+    modal.classList.remove('open')
+}
+
+modalLogin.addEventListener('click', showFormLogin);
+modal.addEventListener('click', hideFormLogin);
+modalBody.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+
+
