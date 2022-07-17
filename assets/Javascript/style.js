675bfa35e91d8347dc1087c1fcee5106428a5106
Nguyen Duc Dung <nguyenducdung13022001.nguyen@gmail.com>
2022-07-17 14:51:29 +0700

    Nguyen Duc Dung

--- assets/Javascript/style.js
+++ assets/Javascript/style.js
@@ -1,3 +1,20 @@
+
+// action like and dislike
+
+ const likes = document.querySelectorAll('.home-product-item__like');
+ for(var i=0;i<=likes.length;i++)
+ {
+  likeElement=likes[i];
+  likeElement.addEventListener('click', function () {
+    if (this.classList.contains('home-product-item__like--liked')) {
+      this.classList.remove('home-product-item__like--liked');
+    } else {
+      this.classList.add('home-product-item__like--liked');
+    }
+  });
+  
+ }
+ 
 // Ẩn hiện form login/Resgiter
 
 const modal = document.querySelector('.js-modal');
